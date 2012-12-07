60b9afd135b3532fea8ad4a948aca17262cf5c65
Kate Hertweck <k8hertweck@gmail.com>
2012-12-07 14:00:25 -0500

    fixed file formatting issues

--- flyfilter.sh
+++ flyfilter.sh
@@ -1,16 +1,16 @@
-# script to filter out paired population frequency data from tlex .csv file which is comma delimited
+# script to filter out paired population frequency data from tlex .csv file which is tab delimited and text is not quoted
 
 #count number of original records
 wc -l $1
 
 #remove lines with NA from a given input file
-grep -v "\"no_data\"" $1 > $1tempNA
+grep -v "no_data" $1 > $1tempNA
 wc -l $1tempNA
 #pull out lines with 0, 0
 grep -v "0,0" $1tempNA > $1temp0
 wc -l $1temp0
 #pull out lines with 100, 100 and replace commas with tabs
-grep -v "100,100" $1temp0 > $1.filter
+grep -v "100,100" $1temp0 | tr '[,]' '[\t]' > $1.filter
 wc -l $1.filter
 
 #remove intermediate files
