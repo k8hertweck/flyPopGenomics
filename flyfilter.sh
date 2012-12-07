d2d768d33d46d7c85ae2aa43061c9133da3e3357
Kate Hertweck <k8hertweck@gmail.com>
2012-12-07 12:26:00 -0500

    replace commas with tab

--- flyfilter.sh
+++ flyfilter.sh
@@ -9,6 +9,13 @@ wc -l $1tempNA
 #pull out lines with 0, 0
 grep -v "0,0" $1tempNA > $1temp0
 wc -l $1temp0
-#pull out lines with 100, 100
-grep -v "100,100" $1temp0 > $1.filter
+#pull out lines with 100, 100 and replace commas with tabs
+grep -v "100,100" $1temp0 | awk 'BEGIN {FS=","; OFS=" "} 
+{print $0}' > $1.filter
 wc -l $1.filter
+
+#remove intermediate files
+rm $1tempNA $temp0
+
+#replace commas with tabs
+
