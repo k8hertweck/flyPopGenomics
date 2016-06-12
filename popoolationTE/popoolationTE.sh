79a2299884b90cf8cc97e59dce5f4f4992e468f7
k8hertweck <k8hertweck@gmail.com>
2016-06-12 00:13:39 -0500

    adding freq commands to main file

--- popoolationTE/popoolationTE.sh
+++ popoolationTE/popoolationTE.sh
@@ -59,4 +59,20 @@ for x in 1 2 3 4 5
 			--min-dist 74 --max-dist 250 --output te-inserts$1"$x".txt \
 			--single-site-shift 100 --poly-n ../poly_n.gtf \
 			--te-hierarchy ../TEhierarchy5.51.tsv --te-hier-level order
+
+		# Use known TE insertions to improve crosslinking 
+		perl $popte/update-teinserts-with-knowntes.pl --known ../TEknown5.51.tsv \
+			--output te-insertions$1"$x".txt --te-hierarchy-file ../TEhierarchy5.51.tsv \
+			--te-hierarchy-level family --max-dist 300 \
+			--te-insertions te-inserts$1"$x".txt --single-site-shift 100
+
+		# estimate population frequencies
+		perl $popte/estimate-polymorphism.pl --sam-file $1"$x"pe-reads.sorted.sam \
+			--te-insert-file te-insertions$1"$x".txt \
+			--te-hierarchy-file ../TEhierarchy5.51.tsv --te-hierarchy-level family \
+			--min-map-qual 15 --output te-polymorphism$1"$x".txt
+
+		# filter output
+		perl $popte/filter-teinserts.pl --te-insertions te-polymorphism$1"$x".txt \
+			--output te-poly-filtered$1"$x".txt --discard-overlapping --min-count 10
 done
