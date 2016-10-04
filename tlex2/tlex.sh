39a1f48a9303892f85217c9f8123a1f8b5ac5577
k8hertweck <k8hertweck@gmail.com>
2016-10-04 13:47:39 -0500

    updating script to match referenced files

--- tlex2/tlex.sh
+++ tlex2/tlex.sh
@@ -8,9 +8,9 @@
 
 cd /home/nescent/kh200/fly
 
-perl tlex-open-v2.2.1.pl -pairends yes -noclean -T annotations/list -M annotations/annotations -G ref/ref.fasta -R ACO
-perl tlex-open-v2.2.1.pl -pairends yes -noclean -T annotations/list -M annotations/annotations -G ref/ref.fasta -R AO
-perl tlex-open-v2.2.1.pl -pairends yes -noclean -T annotations/list -M annotations/annotations -G ref/ref.fasta -R B
-perl tlex-open-v2.2.1.pl -pairends yes -noclean -T annotations/list -M annotations/annotations -G ref/ref.fasta -R BO
-perl tlex-open-v2.2.1.pl -pairends yes -noclean -T annotations/list -M annotations/annotations -G ref/ref.fasta -R CO
-perl tlex-open-v2.2.1.pl -pairends yes -noclean -T annotations/list -M annotations/annotations -G ref/ref.fasta -R NCO
+perl tlex-open-v2.2.1.pl -pairends yes -noclean -T annotations.lst -M 	TEcopies_R542_5425TEs_108TEreannotated_map_strand -G ref/ref.fasta -R ACO
+perl tlex-open-v2.2.1.pl -pairends yes -noclean -T annotations.lst -M 	TEcopies_R542_5425TEs_108TEreannotated_map_strand -G ref/ref.fasta -R AO
+perl tlex-open-v2.2.1.pl -pairends yes -noclean -T annotations.lst -M 	TEcopies_R542_5425TEs_108TEreannotated_map_strand -G ref/ref.fasta -R B
+perl tlex-open-v2.2.1.pl -pairends yes -noclean -T annotations.lst -M 	TEcopies_R542_5425TEs_108TEreannotated_map_strand -G ref/ref.fasta -R BO
+perl tlex-open-v2.2.1.pl -pairends yes -noclean -T annotations.lst -M 	TEcopies_R542_5425TEs_108TEreannotated_map_strand -G ref/ref.fasta -R CO
+perl tlex-open-v2.2.1.pl -pairends yes -noclean -T annotations.lst -M 	TEcopies_R542_5425TEs_108TEreannotated_map_strand -G ref/ref.fasta -R NCO
